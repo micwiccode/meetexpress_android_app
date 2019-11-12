56fb642b8e88b661affac2aeba4883237d05d5df
Maciek <maciek@bartosik.wtf>
2019-11-12 18:33:52 +0100

    Some screens

--- app/src/main/java/com/example/meetexpress/LogInActivity.kt
+++ app/src/main/java/com/example/meetexpress/LogInActivity.kt
@@ -3,10 +3,10 @@ package com.example.meetexpress
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_log_in)
     }
 }
